f6f8906bf7a8cfea528b5ae6da671ae42c11eac6
Planlab <Planlab@DESKTOP-MVKBH79.mshome.net>
2023-07-22 19:45:01 +0500

    vehicle api finalized

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -7,7 +7,6 @@ import {
   Where,
 } from '@loopback/repository';
 import {
-  del,
   get,
   getModelSchemaRef,
   param,
@@ -43,8 +42,10 @@ export class VehicleController {
       },
     })
     vehicle: Omit<Vehicle, 'vehicleId'>,
-  ): Promise<Vehicle> {
-    return this.vehicleRepository.create(vehicle);
+  ): Promise<string> {
+	  const result = {code: 0, msg: "Vehicle created successfully", vehicle: {}};
+    result.vehicle =  this.vehicleRepository.create(vehicle);
+    return JSON.stringify(result);
   }
 
   @get('/vehicles/count')
@@ -95,7 +96,7 @@ export class VehicleController {
     return this.vehicleRepository.updateAll(vehicle, where);
   }
 
-  @get('/vehicles/{id}')
+  @get('/vehicles/getVehicle/{id}')
   @response(200, {
     description: 'Vehicle model instance',
     content: {
@@ -107,12 +108,14 @@ export class VehicleController {
   async findById(
     @param.path.string('id') id: string,
     @param.filter(Vehicle, {exclude: 'where'}) filter?: FilterExcludingWhere<Vehicle>
-  ): Promise<Vehicle> {
-    return this.vehicleRepository.findById(id, filter);
+  ): Promise<string> {
+	  const result = {code: 0, msg: "Vehicle fetched successfully", vehicle: {}};
+    result.vehicle = this.vehicleRepository.findById(id, filter);
+    return JSON.stringify(result);
   }
 
-  @patch('/vehicles/{id}')
-  @response(204, {
+  @post('/vehicles/updateVehicle/{id}')
+  @response(200, {
     description: 'Vehicle PATCH success',
   })
   async updateById(
@@ -125,8 +128,12 @@ export class VehicleController {
       },
     })
     vehicle: Vehicle,
-  ): Promise<void> {
+  ): Promise<string> {
+	  const result = {code: 0, msg: "Vehicle updated successfully.", vehicle: {}};
+	  await this.vehicleRepository.updateById(id, vehicle);
+    result.vehicle = this.vehicleRepository.findById(id, {});
     await this.vehicleRepository.updateById(id, vehicle);
+    return JSON.stringify(result);
   }
 
   @put('/vehicles/{id}')
@@ -140,11 +147,13 @@ export class VehicleController {
     await this.vehicleRepository.replaceById(id, vehicle);
   }
 
-  @del('/vehicles/{id}')
+  @post('/vehicles/deleteVehicle/{id}')
   @response(204, {
     description: 'Vehicle DELETE success',
   })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
+  async deleteById(@param.path.string('id') id: string): Promise<string> {
+	  const result = {code: 0, msg: "Vehicle deleted."};
     await this.vehicleRepository.deleteById(id);
+    return JSON.stringify(result);
   }
 }
