24fdbff0b7c2b0df064e836aa229394aa2639752
Planlab <Planlab@192.168.10.8>
2023-08-24 01:27:58 +0500

    vehicle api updated

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -1,18 +1,13 @@
 import {
-  Count,
-  CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
 } from '@loopback/repository';
 import {
   get,
   getModelSchemaRef,
   param,
-  patch,
   post,
-  put,
   requestBody,
   response,
 } from '@loopback/rest';
@@ -44,19 +39,8 @@ export class VehicleController {
     vehicle: Omit<Vehicle, 'vehicleId'>,
   ): Promise<string> {
 	  const result = {code: 0, msg: "Vehicle created successfully", vehicle: {}};
-    result.vehicle =  this.vehicleRepository.create(vehicle);
+    result.vehicle = await this.vehicleRepository.create(vehicle);
     return JSON.stringify(result);
-  }
-
-  @get('/vehicles/count')
-  @response(200, {
-    description: 'Vehicle model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(Vehicle) where?: Where<Vehicle>,
-  ): Promise<Count> {
-    return this.vehicleRepository.count(where);
   }
 
   @get('/vehicles')
@@ -76,24 +60,27 @@ export class VehicleController {
   ): Promise<Vehicle[]> {
     return this.vehicleRepository.find(filter);
   }
-
-  @patch('/vehicles')
+  
+  @get('/getUserVehicles/{userId}')
   @response(200, {
-    description: 'Vehicle PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Vehicle, {partial: true}),
+    description: 'Array of Vehicle model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Vehicle, {includeRelations: true}),
         },
       },
-    })
-    vehicle: Vehicle,
-    @param.where(Vehicle) where?: Where<Vehicle>,
-  ): Promise<Count> {
-    return this.vehicleRepository.updateAll(vehicle, where);
+    },
+  })
+  async findUserVehicles(
+	  @param.path.string('userId') userId: string,
+    @param.filter(Vehicle) filter?: Filter<Vehicle>,
+  ): Promise<string> {
+	  filter = {...filter, where: {userId: userId}};
+    const result = {code: 0, msg: "Vehicle fetched successfully", vehicle: {}};
+    result.vehicle = await this.vehicleRepository.find(filter);
+    return JSON.stringify(result);
   }
 
   @get('/vehicles/getVehicle/{id}')
@@ -110,7 +97,7 @@ export class VehicleController {
     @param.filter(Vehicle, {exclude: 'where'}) filter?: FilterExcludingWhere<Vehicle>
   ): Promise<string> {
 	  const result = {code: 0, msg: "Vehicle fetched successfully", vehicle: {}};
-    result.vehicle = this.vehicleRepository.findById(id, filter);
+    result.vehicle = await this.vehicleRepository.findById(id, filter);
     return JSON.stringify(result);
   }
 
@@ -131,24 +118,12 @@ export class VehicleController {
   ): Promise<string> {
 	  const result = {code: 0, msg: "Vehicle updated successfully.", vehicle: {}};
 	  await this.vehicleRepository.updateById(id, vehicle);
-    result.vehicle = this.vehicleRepository.findById(id, {});
-    await this.vehicleRepository.updateById(id, vehicle);
+    result.vehicle = await this.vehicleRepository.findById(id, {});
     return JSON.stringify(result);
   }
 
-  @put('/vehicles/{id}')
-  @response(204, {
-    description: 'Vehicle PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() vehicle: Vehicle,
-  ): Promise<void> {
-    await this.vehicleRepository.replaceById(id, vehicle);
-  }
-
   @post('/vehicles/deleteVehicle/{id}')
-  @response(204, {
+  @response(200, {
     description: 'Vehicle DELETE success',
   })
   async deleteById(@param.path.string('id') id: string): Promise<string> {
