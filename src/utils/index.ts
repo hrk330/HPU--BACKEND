9fc717d63c2e4415c2e2126548ed207074f8de22
hrk330 <hrk3341@gmail.com>
2023-09-11 04:58:40 +0500

    Code Clean UP

---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './acc-create-mails.utils';
+// export * from './order-emails.utils';
+export * from './service-orders.utils';
---
+++ src/utils/order-emails.utils.ts
@@ -0,0 +1,41 @@
+// import {ServiceOrders} from '../models';
+
+// export class OrderEmailsUtil {
+//   async sendAcceptOrderEmail(serviceOrder: ServiceOrders): Promise<void> {
+//     console.log('Service Provider Email', serviceOrder.serviceProviderEmail);
+//     console.log('Service Provider Name', serviceOrder.serviceProviderName);
+//     if (serviceOrder.serviceProviderEmail) {
+//     sendAcceptOrderEmailToServiceProvider
+//     }
+//     if(serviceOrder.companyEmail) {
+// sendAcceptOrderEmailToCompany
+//     }
+//   }
+
+//   sendAcceptOrderEmailToServiceProvider;
+
+//       sendCustomMail(
+//         serviceOrder.serviceProviderEmail,
+//         `Current Order Details`,
+//         serviceOrder.serviceProviderName as string,
+//         serviceOrder.serviceOrderId,
+//         serviceOrder.serviceName as string,
+//         'orderCreate',
+//         undefined,
+//         serviceOrder.netAmount,
+//       );
+
+//     sendAcceptOrderEmailToCompany;
+
+//       sendCustomMail(
+//         serviceOrder.serviceProviderEmail,
+//         `Current Order Details`,
+//         serviceOrder.serviceProviderName as string,
+//         serviceOrder.serviceOrderId,
+//         serviceOrder.serviceName as string,
+//         'orderCreate',
+//         undefined,
+//         serviceOrder.netAmount,
+//       );
+
+// }
